1d5697430ee197da25720a41c1f346fffd3fabae
Mayank Mohan Sharma <mayankmohans@zillowgroup.com>
2017-11-01 13:55:46 -0700

    Fix etd array traversal

--- bartetdchecker.py
+++ bartetdchecker.py
@@ -44,7 +44,7 @@ def get_bart_etd():
 		if loop > len(get_count_of_destination()):
 			print "Sorry! It looks like BART is not available for your destination right now!"
 		if destination.text == to_station:
-			for x in root.xpath('/root/station/etd/estimate/minutes'):
+			for x in root.xpath('/root/station/etd[{}]/estimate/minutes'.format(loop)):
 				counter += 1
 				if x.text == "Leaving":
 					print x.text, "Now"
